8d495d85e9768095fff68e6fcabc58c6b10add0e
mimisekai <hyunyoo5064@naver.com>
2023-12-22 11:46:04 +0900

    로그인 기능 구현 완료 1 - 단순 로그인 성공

--- src/main/java/db/dao/MemberInfoDAO.java
+++ src/main/java/db/dao/MemberInfoDAO.java
@@ -10,47 +10,83 @@ import java.util.List;
 import db.dto.MemberInfoDTO;
 import db.util.DBConnectionManager;
 
-
 public class MemberInfoDAO {
 
 	Connection conn;
 	PreparedStatement psmt;
 	ResultSet rs;
-	//멤버정보찾기리스트
-	public List<MemberInfoDTO> findMemberList(){
-		
-				conn = DBConnectionManager.connectDB();
 
-				String sql =  " SELECT * FROM member_info ORDER BY membership_number " ;
-				
-				List<MemberInfoDTO> memberInfoList = null;
-				
-				try {
-					psmt = conn.prepareStatement(sql);
+	// 멤버정보찾기리스트
+	public List<MemberInfoDTO> findMemberList() {
 
-					rs = psmt.executeQuery(); 
-					memberInfoList = new ArrayList<MemberInfoDTO>();
-					
-					while(rs.next()) { 
-						
-						MemberInfoDTO memberInfoDTO = new MemberInfoDTO(
-								rs.getInt("membership_number"), rs.getString("name"), rs.getInt("age"),
-								rs.getString("tel"), rs.getString("id"), rs.getString("password"),
-								rs.getString("email"), rs.getString("membership_level"), rs.getString("lisence_number"),
-								rs.getString("gender"), rs.getString("birthday"), rs.getString("lisence_acquisition_date"),
-								rs.getInt("overdue_history"), rs.getInt("use_count"));
-						
-						memberInfoList.add(memberInfoDTO);
-					}
+		conn = DBConnectionManager.connectDB();
 
-				} catch (SQLException e) {
-					e.printStackTrace();
-				} finally {
-					DBConnectionManager.closeDB(conn, psmt, rs);
-				}
-				
-				
-				return memberInfoList;
+		String sql = " SELECT * FROM member_info ORDER BY membership_number ";
+
+		List<MemberInfoDTO> memberInfoList = null;
+
+		try {
+			psmt = conn.prepareStatement(sql);
+
+			rs = psmt.executeQuery();
+			memberInfoList = new ArrayList<MemberInfoDTO>();
+
+			while (rs.next()) {
+
+				MemberInfoDTO memberInfoDTO = new MemberInfoDTO(rs.getInt("membership_number"), rs.getString("name"),
+						rs.getInt("age"), rs.getString("tel"), rs.getString("id"), rs.getString("password"),
+						rs.getString("email"), rs.getString("membership_level"), rs.getString("lisence_number"),
+						rs.getString("gender"), rs.getString("birthday"), rs.getString("lisence_acquisition_date"),
+						rs.getInt("overdue_history"), rs.getInt("use_count"));
+
+				memberInfoList.add(memberInfoDTO);
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			DBConnectionManager.closeDB(conn, psmt, rs);
+		}
+
+		return memberInfoList;
 	}
-	
+
+	// 고객이 입력한 id랑 pw가 서버에 똑같은 게 있나 확인하는 클래스
+	public List<MemberInfoDTO> findMemberListById(String targetId, String targetPw) {
+		conn = DBConnectionManager.connectDB();
+
+		String sql = " SELECT * FROM member_info WHERE id = ? and password = ? ";
+
+		System.out.println(targetId + targetPw);
+		
+		List<MemberInfoDTO> memberInfoList = null;
+
+		try {
+			psmt = conn.prepareStatement(sql);
+			psmt.setString(1, targetId);
+			psmt.setString(2, targetPw);
+			rs = psmt.executeQuery();
+			memberInfoList = new ArrayList<MemberInfoDTO>(); // 못 찾아도 null이 아님
+
+			while (rs.next()) {
+
+				MemberInfoDTO memberInfoDTO = new MemberInfoDTO(rs.getInt("membership_number"), rs.getString("name"),
+						rs.getInt("age"), rs.getString("tel"), rs.getString("id"), rs.getString("password"),
+						rs.getString("email"), rs.getString("membership_level"), rs.getString("lisence_number"),
+						rs.getString("gender"), rs.getString("birthday"), rs.getString("lisence_acquisition_date"),
+						rs.getInt("overdue_history"), rs.getInt("use_count"));
+
+				memberInfoList.add(memberInfoDTO);
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			DBConnectionManager.closeDB(conn, psmt, rs);
+		}
+
+		return memberInfoList;
+
+	}
+
 }
