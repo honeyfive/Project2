5ad7d8cfd24b8a6609012ea4bfebed6429c65066
mimisekai <hyunyoo5064@naver.com>
2024-01-04 16:19:08 +0900

    마이페이지 - 예약취소 기능 완성

--- src/main/webapp/js/mypage.js
+++ src/main/webapp/js/mypage.js
@@ -12,15 +12,5 @@ ratingModalClose.addEventListener("click",()=>{ // 모달 안 보이게
 	ratingModal.style.display = "none";
 })
 
-// 이용내역 모달창 버튼
-let useModalOpen = document.querySelector(".use_count_box");
-let useModal = document.querySelector(".modal_use");
-let useModalClose = document.querySelector(".modal_use_close_btn");
 
-useModalOpen.addEventListener("click",()=>{ // 모달 안 보이게
-	useModal.style.display = "flex";
-})
 
-useModalClose.addEventListener("click",()=>{ // 모달 안 보이게
-	useModal.style.display = "none";
-})
